e23fcf87bf954cdf02ca57b6c650ea3648a670c2
Federico Mendez <fede.bd@gmail.com>
2018-03-18 14:00:48 -0300

    Functions completed. Scopes missing

--- 06_functions_and_scopes.py
+++ 06_functions_and_scopes.py
@@ -1,4 +1,5 @@
-# Las funciones son la sal de la vida de la programación. Permiten escribir código complejo una vez y reutilizarlo varias veces
+# Las funciones son la sal de la vida de la programación. Permiten escribir código complejo una vez y reutilizarlo varias veces.
+# Así como en matemática, las funciones toman parámetros y devuelven valores.
 # Por ejemplo, si tomamos el algoritmo para sumar los primeros n números naturales y lo queremos aplicar sobre dos números distintos
 n = int(input('Hasta donde sumar? '));
 suma = 0;
@@ -19,11 +20,58 @@ def sumaNatural(hastaDonde):
     for iterador in range(1, hastaDonde + 1):
         sumaInterna = sumaInterna + iterador
 
-    print('La suma de los primeros', hastaDonde, 'naturales da un total de', sumaInterna);
-
     return sumaInterna;
 
 n = int(input('Hasta donde sumar? '));
-sumaNatural(n);
+suma = sumaNatural(n);
+print('La suma de los primeros', n, 'naturales da un total de', suma);
 n = int(input('Hasta donde sumar ahora? '));
 sumaNatural(n);
+print('La suma de los primeros', n, 'naturales da ahora un total de', suma);
+# 
+# Las funciones pueden tomar cualquier cantidad de parámetros, y hasta algunos opcionales
+# Así como los parámetros son opcionales, también lo es la devolución de valores
+def categoria(club, categoria):
+    if club != 'BOCA':
+        print(club, 'sos de la B');
+    else:
+        print(club, 'sos de la', categoria);
+    return;
+
+categoria('riber', 'a');
+categoria('BOCA', 'A');
+
+def categoria2(club, categoria = 'A'):
+    if club != 'BOCA':
+        print(club, 'sos de la B');
+    else:
+        print(club, 'sos de la', categoria);
+    return;
+
+categoria2('riber', 'A');
+categoria2('BOCA');
+#
+# Podemos también llamar a funciones dentro de funciones, para hacer más sencilla la lectura
+# Si por ejemplo tenemos una funcion que recorre un arreglo de naturales y devuelve la suma de los n primeros números naturales
+def sumaFea(arreglo):
+    for elemento in arreglo:
+        sumaInterna = 0;
+        for indice in range(1, elemento+1):
+            sumaInterna = sumaInterna + indice;
+        print('La suma de los primeros', elemento, 'naturales da un total de', sumaInterna);
+    return;
+
+muchos = range(1, 6);
+sumaFea(muchos);
+#
+# Pero ya teníamos una función que realiza la suma natural de un número, así que podemos llamarla dentro de la propia función
+def sumaFactorizada(arreglo):
+    for elemento in arreglo:
+        resultado = sumaNatural(elemento);
+        print('La suma de los primeros', elemento, 'naturales da un total de', resultado);
+    return;
+
+sumaFactorizada(muchos);
+#
+# Veremos ahora el _scope_ de las variables
+# Las variables definidas "en el aire" (fuera de funciones, ifs, ciclos, etc) se llaman variables GLOBALES
